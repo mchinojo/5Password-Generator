982f2a76780fd0000232dcdd9e2cbea6254f49fa
Coni Hinojosa <coni@Conis-MacBook-Pro.local>
2023-01-06 14:36:05 +0000

    completed main task on javascript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+let specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+let lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+let upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -90,29 +90,58 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let userLengthOpt = Number(window.prompt("Please choose the number of characters for your password. It must have at least 10 characters, but no more than 64.", 10));
+  let userLowercaseOpt = window.confirm("Do you want your password with lower-cases?");
+  let userUppercaseOpt = window.confirm("Do you want your password with upper-cases?");
+  let userNumericOpt = window.confirm("Do you want your password with numbers?");
+  let userSpecialCharOpt = window.confirm("Do you want your password with special characters?");
 
+
+  return { userLengthOpt, userLowercaseOpt, userUppercaseOpt, userNumericOpt, userSpecialCharOpt };
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(passwordOptions) {
+  let password = "";
+  let selectedChars = [];
 
+  if (passwordOptions.userLowercaseOpt) {
+    selectedChars.push(lowerCasedCharacters);
+  }
+  if (passwordOptions.userUppercaseOpt) {
+    selectedChars.push(upperCasedCharacters);
+  }
+  if (passwordOptions.userNumericOpt) {
+    selectedChars.push(numericCharacters);
+  }
+  if (passwordOptions.userSpecialCharOpt) {
+    selectedChars.push(specialCharacters);
+  }
+
+  for (let index = 0; index < passwordOptions.userLengthOpt; index++) {
+    password += getRandom(getRandom(selectedChars));
+  }
+  return password;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let passwordOptions = getPasswordOptions();
+  console.log(passwordOptions);
+  let password = generatePassword(passwordOptions);
+  let displayPassword = document.querySelector('#password');
 
-  passwordText.value = password;
+  displayPassword.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
